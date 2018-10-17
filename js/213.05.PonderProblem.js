48a0e5cdd5b29ec398ce7f3d9d991701d006a64f
Tony Moraes <tonylds1@gmail.com>
2018-10-17 06:05:55 -0300

    commentaries and precision configuration

--- js/213.05.PonderProblem.js
+++ js/213.05.PonderProblem.js
@@ -1,8 +1,16 @@
+/**
+ * Hello World
+ */
 function problem1() {
     console.log("Hello World!");
     alert("Hello World!");
 }
 
+/**
+ * Replace a string inside a letter text
+ * @param {String} name 
+ * @param {String} letter 
+ */
 function problem2(name, letter) {
     var newLetter = letter.replace(/RICH_GUY/g, name);
 
@@ -10,13 +18,21 @@ function problem2(name, letter) {
     alert(newLetter);
 }
 
+/**
+ * Convert Fahreinghert to Celcius
+ * @param {Number} fahrenheit 
+ */
 function problem3(fahrenheit) {
     var result = (fahrenheit - 32) * 5/9;
 
-    console.log(result.toFixed(2));
-    alert(result.toFixed(2));
+    console.log(result.toFixed(1));
+    alert(result.toFixed(1));
 }
 
+/**
+ * Validate if the number is some between 1 and 5
+ * @param {Number} weight 
+ */
 function problem4(weight) {
     if (weight < 1 || weight > 5) {
         console.log('Invalid weight');
@@ -27,11 +43,19 @@ function problem4(weight) {
     alert('Good weight');
 }
 
+/**
+ * Use a loop to calculate compound interest
+ * @param {Number} apr 
+ * @param {Number} term 
+ * @param {Number} amount 
+ */
 function problem5(apr, term, amount) {
-    for (i = 0; i < term * 12; i++) {
-        amount = amount * (1 + apr/1200);
+    var result = Number(amount);
+    
+    for (i = 0; i < Number(term); i++) {
+        result = result * (1 + Number(apr)/100);
     }
 
-    console.log(amount.toFixed(2));
-    alert(amount.toFixed(2));
+    console.log(result.toFixed(2));
+    alert(result.toFixed(2));
 }

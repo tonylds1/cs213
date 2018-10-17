03132b29bc5e30a7192372fb74d1c177c9bfb3e5
Tony Moraes <tonylds1@gmail.com>
2018-10-17 05:52:10 -0300

    W05 Ponder

--- js/213.05.PonderProblem.js
+++ js/213.05.PonderProblem.js
@@ -1,7 +1,37 @@
 function problem1() {
     console.log("Hello World!");
+    alert("Hello World!");
 }
 
 function problem2(name, letter) {
-    letter.replace()
+    var newLetter = letter.replace(/RICH_GUY/g, name);
+
+    console.log(newLetter);
+    alert(newLetter);
+}
+
+function problem3(fahrenheit) {
+    var result = (fahrenheit - 32) * 5/9;
+
+    console.log(result.toFixed(2));
+    alert(result.toFixed(2));
+}
+
+function problem4(weight) {
+    if (weight < 1 || weight > 5) {
+        console.log('Invalid weight');
+        alert('Invalid weight');
+    }
+
+    console.log('Good weight');
+    alert('Good weight');
+}
+
+function problem5(apr, term, amount) {
+    for (i = 0; i < term * 12; i++) {
+        amount = amount * (1 + apr/1200);
+    }
+
+    console.log(amount.toFixed(2));
+    alert(amount.toFixed(2));
 }
